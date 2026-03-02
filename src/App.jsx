9db5dd19dc0f6a8034e2907ba2b178dcac828132
Artem Kutsan <artemkutsan@gmail.com>
2026-03-02 13:04:02 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className="container">React 27 Practice 11</h1>
+
+        <section>
+          <div className="container">
+            <h2>Задача 1: Роутинг react-router-dom</h2>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
